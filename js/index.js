8d58c38839e39c63a0833086a4c01ed74647ea4d
Adam777 <Adam777-5867@outlook.com>
2022-10-04 23:33:48 +0000

    Update

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ var tooltip;
 var svg;
 
 const colorScale = d3.scaleOrdinal()
-						.range(schemeCategory20);
+								.range(schemeCategory20);
 
 document.addEventListener('DOMContentLoaded', function(event) {
 	tooltip = d3.select('body')
@@ -26,9 +26,9 @@ document.addEventListener('DOMContentLoaded', function(event) {
 								.attr('id', 'tooltip');
 
 	svg = d3.select('#canvas')
-							.append('svg')
-							.attr('width', w)
-							.attr('height', h);
+								.append('svg')
+								.attr('width', w)
+								.attr('height', h);
 
 	Promise.all([KickstarterPledgesURL, MovieSalesURL, VideoGameSalesURL].map(url => d3.json(url))).then(function(data) {
 		dataset['KickstarterPledges'] = data[0];
@@ -58,17 +58,17 @@ function createTreemap(datasetName = 'KickstarterPledges') {
 	svg.html('');
 
 	const treemap = d3.treemap()
-										.size([w, h-legendH]);
+								.size([w, h-legendH]);
 
 	const tree = d3.hierarchy(dataset[datasetName])
-					.sum(d => d['value'])
-					.sort((a, b) => b.height - a.height || b.value - a.value);
+								.sum(d => d['value'])
+								.sort((a, b) => b.height - a.height || b.value - a.value);
 
 	treemap(tree);
 
 	const legend = svg.append('g')
-						.attr('id', 'legend')
-						.attr('transform', 'translate(' + (w / 10.7) + ', 0)');
+								.attr('id', 'legend')
+								.attr('transform', 'translate(' + (w / 10.7) + ', 0)');
 
 	let categories = tree.leaves().map(e => e['data']['category']).filter((value, index, self) => self.indexOf(value) === index);
 
@@ -84,48 +84,48 @@ function createTreemap(datasetName = 'KickstarterPledges') {
 								.attr('transform', (d, i) => 'translate(' + ((i % legendElementsPerRow) * legendHSpace) + ', ' + ((Math.floor(i / legendElementsPerRow)) * legendRectSize + (legendVSpace * (Math.floor(i / legendElementsPerRow)))) + ')');
 
 	legendElement.append('rect')
-					.attr('class', 'legend-item')
-					.attr('width', legendRectSize)
-					.attr('height', legendRectSize)
-					.attr('fill', (d) => colorScale(d));
+	.attr('class', 'legend-item')
+	.attr('width', legendRectSize)
+	.attr('height', legendRectSize)
+	.attr('fill', (d) => colorScale(d));
 
 	legendElement.append('text')
-				.attr('x', legendRectSize + 3)
-				.attr('y', legendRectSize - 2)
-				.text(d => d);
+	.attr('x', legendRectSize + 3)
+	.attr('y', legendRectSize - 2)
+	.text(d => d);
 
 	let treeMap = svg.append('g')
-					.attr('id', 'tree-map')
-					.attr('transform', 'translate(0, ' + (legendH + 10) + ')');
+								.attr('id', 'tree-map')
+								.attr('transform', 'translate(0, ' + (legendH + 10) + ')');
 
 	let group = treeMap.selectAll('g')
-									.data(tree.leaves())
-									.enter()
-									.append('g')
-										.attr('class', 'group')
-										.attr('transform', (d) => 'translate(' + d['x0'] + ', ' + d['y0'] + ')');
+								.data(tree.leaves())
+								.enter()
+								.append('g')
+									.attr('class', 'group')
+									.attr('transform', (d) => 'translate(' + d['x0'] + ', ' + d['y0'] + ')');
 
 	group.append('rect')
-				.attr('width', (d, i) => d['x1'] - d['x0'])
-				.attr('height', (d, i) => d['y1'] - d['y0'])
-				.attr('fill', d => colorScale(d['data']['category']))
-				.attr('class', 'tile')
-				.attr('data-name', (d, i) => d['data']['name'])
-				.attr('data-category', (d, i) => d['data']['category'])
-				.attr('data-value', (d, i) => d['data']['value'])
-				// .append('title')
-				// .text(d['data']['name'] + ' (' + d['data']['category'] + '): ' + d['data']['value'])
-				.on('mouseover', (d) => tooltip.style('display', 'block').attr('data-value', d['value']).text(d['data']['name'] + ' (' + d['data']['category'] + '): ' + d['data']['value']))
-				.on('mousemove', () => tooltip.style('top', (d3.event.pageY - 35) + 'px').style('left', (d3.event.pageX + 5) + 'px'))
-				.on('mouseout', () => tooltip.style('display', 'none'));
+	.attr('width', (d, i) => d['x1'] - d['x0'])
+	.attr('height', (d, i) => d['y1'] - d['y0'])
+	.attr('fill', d => colorScale(d['data']['category']))
+	.attr('class', 'tile')
+	.attr('data-name', (d, i) => d['data']['name'])
+	.attr('data-category', (d, i) => d['data']['category'])
+	.attr('data-value', (d, i) => d['data']['value'])
+	// .append('title')
+	// .text(d['data']['name'] + ' (' + d['data']['category'] + '): ' + d['data']['value'])
+	.on('mouseover', (d) => tooltip.style('display', 'block').attr('data-value', d['value']).text(d['data']['name'] + ' (' + d['data']['category'] + '): ' + d['data']['value']))
+	.on('mousemove', () => tooltip.style('top', (d3.event.pageY - 35) + 'px').style('left', (d3.event.pageX + 5) + 'px'))
+	.on('mouseout', () => tooltip.style('display', 'none'));
 
 	group.append('text')
-				.attr('class', 'tile-text')
-				.selectAll('tspan')
-				.data(d => d['data']['name'].split(/\s/g))
-				.enter()
-				.append('tspan')
-					.attr('x', 5)
-					.attr('y', (d, i) => 10 + (i * 10))
-					.text(d => d);
+	.attr('class', 'tile-text')
+	.selectAll('tspan')
+	.data(d => d['data']['name'].split(/\s/g))
+	.enter()
+	.append('tspan')
+		.attr('x', 5)
+		.attr('y', (d, i) => 10 + (i * 10))
+		.text(d => d);
 }
