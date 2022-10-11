640189a5d4a1e2fd596b3c6647a1f29067c573d6
Adam777 <Adam777-5867@outlook.com>
2022-10-11 22:54:12 +0000

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,9 @@ localStorage.setItem('example_project', 'D3: Tree Map');
 
 const schemeCategory20 = ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5'];
 
-const KickstarterPledgesURL = './json/kickstarter-funding-data.json';
-const MovieSalesURL = './json/movie-data.json';
-const VideoGameSalesURL = './json/video-game-sales-data.json';
+const KickstarterPledgesURL = './assets/json/kickstarter-funding-data.json';
+const MovieSalesURL = './assets/json/movie-data.json';
+const VideoGameSalesURL = './assets/json/video-game-sales-data.json';
 
 const dataset = {};
 var datasetName = 'KickstarterPledges';
